4f8be43f92ccb2fb05cb25b4a01b52dedd2b1bb0
Owen Kirby <oskirby@gmail.com>
2019-07-14 18:23:22 -0700

    Add parsing of color data from JSON.
    The JSON animation format now accepts two new encodings for color data:
    rgb8 and palette.
    
    The first format is identified by a dict key of 'rgb8' containing the
    frame data with each pixel given by a pair of hexadecimal characters,
    and rows delimited by colons. The colorspace used in this format gives
    3-bits of red, 3-bits of green and 2-bits of blue.
    
    The second format is identified by a dict key of 'palette' containing
    the frame data, with each pixel given by a single exadecimal nibble,
    and rows delimited by colons. This format uses a palette consisting of
    the 16 xterm system colors (0-15).
    
    8-Bit RGB: https://en.wikipedia.org/wiki/8-bit_color
    Xterm Colors: https://jonasjacek.github.io/colors/

--- animations/__init__.py
+++ animations/__init__.py
@@ -19,11 +19,35 @@ import dcfurs
 
 ## Template class for JSON-encoded animations
 class __jsonanim__:
+    # XTerm color palette
+    xterm = [
+        0x00,   # black
+        0x80,   # maroon
+        0x10,   # green
+        0x90,   # olive
+        0x02,   # navy
+        0x82,   # purple
+        0x12,   # teal
+        0xdb,   # silver
+        0x92,   # grey
+        0xe0,   # red
+        0x1c,   # lime
+        0xfc,   # yellow
+        0x03,   # blue
+        0xe3,   # fuschia
+        0x1f,   # aqua
+        0xff,   # white
+    ]
+
+    # Monochrome gamma intensity mapping
+    intensity = [
+        0, 2, 3, 4, 6, 9, 12, 17, 24, 34, 47, 66, 92, 130, 182, 255
+    ]
+
     def __init__(self):
         fh = open(self.path, "r")
         self.framenum = 0
         self.js = ujson.load(fh)
-        self.intensity = bytearray([0, 2, 3, 4, 6, 9, 12, 17, 24, 34, 47, 66, 92, 130, 182, 255])
         fh.close()
         self.draw()
 
@@ -31,13 +55,47 @@ class __jsonanim__:
         self.interval = int(frame['interval'])
         x = 0
         y = 0
-        for ch in frame['frame']:
-            if ch == ':':
-                x = 0
-                y = y+1
-            else:
-                dcfurs.set_pixel(x,y,self.intensity[int(ch, 16)])
-                x = x+1
+
+        # Handle monochrome and legacy animations
+        if 'frame' in frame:
+            data = frame['frame']
+            for ch in data:
+                if ch == ':':
+                    x = 0
+                    y = y+1
+                else:
+                    dcfurs.set_pixel(x,y,self.intensity[int(ch, 16)])
+                    x = x+1
+        # Handle 8-bit RGB data
+        elif 'rgb8' in frame:
+            pix = 0
+            even = True
+            data = frame['rgb8']
+            for ch in data:
+                if ch == ':':
+                    x = 0
+                    y = y+1
+                elif even:
+                    even = False
+                    pix = int(ch, 16) << 4
+                else:
+                    even = True
+                    pix += int(ch, 16)
+                    dcfurs.set_pixel(x,y,pix)
+                    x = x+1
+        # Handle 4-bit palette data
+        elif 'palette' in frame:
+            data = frame['palette']
+            for ch in data:
+                if ch == ':':
+                    x = 0
+                    y = y+1
+                else:
+                    dcfurs.set_pixel(x,y,self.xterm[int(ch, 16)])
+                    x = x+1
+        # Otherwise, we couldn't make sense of it.
+        else:
+            dcfurs.clear()
 
     def draw(self):
         self.drawframe(self.js[self.framenum])
